644b24b65bd7c1dda049da7495700f9dd0cfd8b0
Code4Data24 <rinkuguptayt457@gmail.com>
2026-01-30 22:36:21 +0530

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,51 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import workspaceRouter from "./routes/workspaceRoutes.js";
+import projectRouter from "./routes/projectRoutes.js";
+import taskRouter from "./routes/taskRoutes.js";
+import commentRouter from "./routes/commentRoutes.js";
+import { protect } from './middlewares/authMiddleware.js';
+import { clerkMiddleware } from '@clerk/express';
+import { inngest, functions } from './inngest/index.js';
+import { serve } from "inngest/express";
+
+const app = express();
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://projectmgt-ten.vercel.app"
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization"
+  ]
+}));
+
+
+app.use(express.json());
+app.use(clerkMiddleware());
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
+app.get('/', (req, res) => res.send('Server is live!'));
+
+// Webhooks
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+// Routes
+app.use("/api/workspaces", protect, workspaceRouter);
+app.use("/api/projects", protect, projectRouter);
+app.use("/api/tasks", protect, taskRouter);
+app.use("/api/comments", protect, commentRouter);
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
